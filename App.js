a4536d4579ba4c558be8d9205ae1dcd91d1190d6
Mayank <raminamayank16@gmail.com>
2020-06-15 13:54:39 +0530

    App.js change

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,101 +14,71 @@ import {
   View,
   Text,
   StatusBar,
+  TextInput,
+  ActivityIndicator,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+class App extends Component {
+  state = {
+    inputText: null,
+    movieData: null,
+    isLoading: false,
+  };
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
+  styles = StyleSheet.create({
+    scrollView: {
+      backgroundColor: '#fff',
+    },
+  });
+
+  onInputChange = input => {
+    this.setState({
+      inputText: input,
+    });
+  };
+  onSubmit = async () => {
+    if (this.state.inputText) {
+      this.setState({
+        isLoading: true,
+      });
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=12506b1b&t=${encodeURIComponent(
+          this.state.inputText,
+        )}`,
+      );
+      console.log(await response.json());
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={this.styles.scrollView}
+          />
+          <TextInput
+            // onChangeText={this.onInputChange}
+            value={this.state.inputText}
+            placeholder="Search Movie"
+            onChangeText={this.onInputChange}
+            onSubmitEditing={this.onSubmit}
+          />
+          {this.state.isLoading ? (
+            <ActivityIndicator />
+          ) : (
+            this.state.movieData && (
+              <View>
+                <Text>Hello!</Text>
+              </View>
+            )
           )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+        </SafeAreaView>
+      </>
+    );
+  }
+}
 
 export default App;
